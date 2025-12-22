1ae021a2152546e7d71bbb3ef8b843fa69bdb5c2
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2025-12-22 01:42:05 -0400

    fix: reduce modal transition durations for improved responsiveness

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -28,10 +28,10 @@ export default function Modal({
       <Dialog as="div" className="relative z-10" onClose={close}>
         <TransitionChild
           as={Fragment}
-          enter="ease-out duration-300"
+          enter="ease-out duration-150"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="ease-in duration-200"
+          leave="ease-in duration-150"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
