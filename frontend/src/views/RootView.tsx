9313532d562786433a354fbf1f623af034db0451
Lorelvis Santos <santoslorelvis@gmail.com>
2025-12-21 21:14:46 -0400

    refactor: improve code readability and structure in DirectoryView component

--- frontend/src/views/RootView.tsx
+++ frontend/src/views/RootView.tsx
@@ -10,28 +10,44 @@ export default function DirectoryView() {
     retry: 1,
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error loading files.</div>;
-  if (data)
-    return (
-      <>
-        <div className="flex flex-col gap-4">
-          <div className="flex items-center justify-between h-14 px-4 shrink-0 mb-4">
-            <h1 className="text-xl font-bold leading-none">My Files</h1>
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-            <div className="flex items-center gap-3">
-              <button className="flex items-center gap-3 bg-night-primary hover:bg-night-primary-hover hover:cursor-pointer transition duration-200 rounded-lg py-2 px-4 text-night-text font-semibold">
-                <FaFolderPlus size={18} />
-                Create Folder
-              </button>
-            </div>
+  if (error) {
+    return <div>Error loading files.</div>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  data.sort((a, b) => {
+    if (a.isDir === b.isDir) {
+      return a.name.localeCompare(b.name);
+    }
+    return a.isDir ? -1 : 1;
+  });
+
+  return (
+    <>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-between h-14 px-4 shrink-0 mb-4">
+          <h1 className="text-xl font-bold leading-none">My Files</h1>
+
+          <div className="flex items-center gap-3">
+            <button className="flex items-center gap-3 bg-night-primary hover:bg-night-primary-hover hover:cursor-pointer transition duration-200 rounded-lg py-2 px-4 text-night-text font-semibold">
+              <FaFolderPlus size={18} />
+              Create Folder
+            </button>
           </div>
         </div>
+      </div>
 
-        {/* El contenedor de la tabla crece para ocupar el resto del espacio */}
-        <div className="flex-1 min-h-0">
-          <FileTable nodes={data} />
-        </div>
-      </>
-    );
+      {/* El contenedor de la tabla crece para ocupar el resto del espacio */}
+      <div className="flex-1 min-h-0">
+        <FileTable nodes={data} />
+      </div>
+    </>
+  )
 }
